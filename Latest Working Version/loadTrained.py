6fdbbd3abc4c084ca837adabd348ec3c2a6cbfeb
krispal <kb2117@ic.ac.uk>
2018-01-28 05:15:04 +0000

    Keras implementation Update 4 - Krispal

--- Latest Working Version/loadTrained.py
+++ Latest Working Version/loadTrained.py
@@ -6,7 +6,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 
-batchSize = 100
+test_data = np.array([['18', 'Male']])
+
+print(test_data)
+
+ageLower = 0
+ageUpper = 60
+genderDict = {'Male': 0, 'Female': 1, 'Other': 0.5}
+movies = ['Inception', 'The Matrix', 'Star Wars', 'Twilight', 'The Godfather', 'Harry Potter', 'Lord of the Rings',
+          'Die Hard', 'The Fault in our Stars', 'Deadpool', 'IT', 'Alien', 'Saw', "Shawshank Redmemption",
+          "The Hunger Games", "12 Angry Men", "Inside Out", "The Incredibles", "The Dark Knight", "Despicable Me",
+          "Inside Out", "Hot Fuzz", "Rush Hour", "Shaun of the dead", "Pitch Perfect"]
 
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
@@ -15,4 +25,13 @@ model = model_from_json(loaded_model_json)
 model.load_weights("model.h5")
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-output = model.predict(training_set_raw_inputs, batch_size=batchSize)
+raw_input = [[int(x[0]) / (ageUpper - ageLower), genderDict[x[1]]] for x in test_data]
+training_set_raw_inputs = np.array(raw_input).astype(float)
+
+
+output = model.predict(training_set_raw_inputs, batch_size=1)
+outputIndex = [np.argmax(x) for x in output]
+
+result = movies[outputIndex[0]]
+
+print(result)
